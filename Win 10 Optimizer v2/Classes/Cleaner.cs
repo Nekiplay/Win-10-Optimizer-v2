e059f3d6cce84953703c74ba5033a52472988f09
Neki play <Herobrine858@outlook.com>
2021-05-12 09:59:52 +0500

    Добавьте файлы проекта.

---
+++ Win 10 Optimizer v2/Classes/Cleaner.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win_10_Optimizer_v2.Classes
+{
+    public class Cleaner
+    {
+        private readonly List<ClearSettings> Logs = new List<ClearSettings>();
+
+        public void UpdateDataBase()
+        {
+            using (WebClient wc = new WebClient())
+            {
+
+            }
+        }
+
+        public class ClearSettings
+        {
+            /* File Settings */
+            public string FilePath;
+            public string Expansion;
+
+
+            public string DirectoryPath;
+            public Modes Mode;
+            public enum Modes
+            {
+                Files,
+                Directory,
+            }
+            public bool IsExists
+            {
+                get
+                {
+                    if (Mode == Modes.Files)
+                    {
+                        if (File.Exists(FilePath)) { return true; }
+                        else { return false; }
+                    }
+                    else if (Mode == Modes.Directory)
+                    {
+                        if (Directory.Exists(DirectoryPath)) { return true; }
+                        else { return false; }
+                    }
+                    else { return false; }
+                }
+            }
+            public void Clear()
+            {
+                if (IsExists && Mode == Modes.Files)
+                {
+                    var result = System.IO.Directory.EnumerateFiles(FilePath, Expansion);
+                    if (result.Count() != 0)
+                    {
+                        foreach (var m in result)
+                        {
+                            try
+                            {
+                                System.IO.FileInfo file = new System.IO.FileInfo(m);
+                                System.IO.File.Delete(m);
+                            }
+                            catch { }
+                        }
+                    }
+                }
+                else if (IsExists && Mode == Modes.Directory)
+                {
+                    System.IO.DirectoryInfo myDirInfo = new System.IO.DirectoryInfo(DirectoryPath);
+                    foreach (System.IO.FileInfo file in myDirInfo.GetFiles()) 
+                    { 
+                        try 
+                        { 
+                            file.Delete(); 
+                        } catch { } 
+                    }
+                    foreach (System.IO.DirectoryInfo diri in myDirInfo.GetDirectories()) 
+                    { 
+                        try 
+                        {
+                            diri.Delete(true); 
+                        } catch 
+                        { } 
+                    }
+                    try { System.IO.Directory.Delete(DirectoryPath, true); } catch { }
+                }
+            }
+        }
+    }
+}
---
+++ Win 10 Optimizer v2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Win_10_Optimizer_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 46);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(51)))), ((int)(((byte)(61)))));
+            this.panel2.Controls.Add(this.guna2Button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(130, 404);
+            this.panel2.TabIndex = 1;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(51)))), ((int)(((byte)(61)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(0, 6);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(130, 24);
+            this.guna2Button1.TabIndex = 2;
+            this.guna2Button1.Text = "guna2Button1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Win 10 Optimizer";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(6)))), ((int)(((byte)(6)))));
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(130, 46);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(670, 404);
+            this.panelDesktop.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelDesktop;
+    }
+}
+
